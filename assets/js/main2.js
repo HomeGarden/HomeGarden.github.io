b6d13ca4b95241726b1799c767a7f4f6be33f6d4
HomeGarden <>
2018-12-02 22:02:48 +0100

    55

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -12,6 +12,8 @@
       var dataField7; /* Thinkspeak data: Command to device 1*/
       var dataField8; /* Thinkspeak data: Command to device 2*/
 
+      var field8;
+
       var localDate;
       var localTemp;
       var generalCond;
@@ -208,16 +210,20 @@
       }
 
       function getDataField8() {       
-        $.getJSON('https://api.thingspeak.com/channels/'+channelID+'/field/8/last.json?apikey='+readKey+'&callback=?', function(data) {          
-            dataField8 = data.field8;
+        $.getJSON('https://api.thingspeak.com/channels/'+channelID+'/field/7/last.json?apikey='+readKey+'&callback=?', function(data) {          
+            field8= data.entry_id;
             if (dataField8) {
                 ddataField8 = (dataField8/1);
             }          
         });
-        return dataField8;
+        return field8;
       }
 
-   
+
+
+
+      document.getElementById("pp").innerHTML = getDataField8();
+
 
       function drawChart() {
 
@@ -483,112 +489,9 @@
          gg5.refresh(getDataField5());
           gg6.refresh(getDataField6());
           gg7.refresh(getDataField7());
+          getDataField8();
       }, 5000);
 
   });
 
-
-
-         FusionCharts.ready(function(){
-    var fusioncharts = new FusionCharts({
-    type: 'thermometer',
-    renderAt: 'chart-container',
-    width: '240',
-    height: '310',
-    dataFormat: 'json',
-    dataSource: {
-        "chart": {
-            "caption": "Temperature Monitor",
-            "subcaption": " Central cold store",
-            "lowerLimit": "-10",
-            "upperLimit": "0",
-
-            "decimals": "1",
-            "numberSuffix": "°C",
-            "showhovereffect": "1",
-            "thmFillColor": "#008ee4",
-            "showGaugeBorder": "1",
-            "gaugeBorderColor": "#008ee4",
-            "gaugeBorderThickness": "2",
-            "gaugeBorderAlpha": "30",
-            "thmOriginX": "100",
-            "chartBottomMargin": "20",
-            "valueFontColor": "#000000",
-            "theme": "fusion"
-        },
-        "value": "-6",
-        //All annotations are grouped under this element
-        "annotations": {
-            "showbelow": "0",
-            "groups": [{
-                //Each group needs a unique ID
-                "id": "indicator",
-                "items": [
-                    //Showing Annotation
-                    {
-                        "id": "background",
-                        //Rectangle item
-                        "type": "rectangle",
-                        "alpha": "50",
-                        "fillColor": "#AABBCC",
-                        "x": "$gaugeEndX-40",
-                        "tox": "$gaugeEndX",
-                        "y": "$gaugeEndY+54",
-                        "toy": "$gaugeEndY+72"
-                    }
-                ]
-            }]
-
-        },
-    },
-    "events": {
-        "rendered": function(evt, arg) {
-            evt.sender.dataUpdate = setInterval(function() {
-                var value,
-                    prevTemp = evt.sender.getData(),
-                    mainTemp = getDataField1(),
-                    diff = Math.abs(prevTemp - mainTemp);
-
-                diff = 0;
-                if (mainTemp > prevTemp) {
-                    value = prevTemp + diff;
-                } else {
-                    value = prevTemp - diff;
-                }
-
-                evt.sender.feedData("&value=" + value);
-
-            }, 3000);
-            updateAnnotation = function(evtObj, argObj) {
-                var code,
-                    chartObj = evtObj.sender,
-                    val = chartObj.getData(),
-                    annotations = chartObj.annotations;
-
-                if (val >= -4.5) {
-                    code = "#00FF00";
-                } else if (val < -4.5 && val > -6) {
-                    code = "#ff9900";
-                } else {
-                    code = "#ff0000";
-                }
-                annotations.update("background", {
-                    "fillColor": code
-                });
-            };
-        },
-        'renderComplete': function(evt, arg) {
-            updateAnnotation(evt, arg);
-        },
-        'realtimeUpdateComplete': function(evt, arg) {
-            updateAnnotation(evt, arg);
-        },
-        'disposed': function(evt, arg) {
-            clearInterval(evt.sender.dataUpdate);
-        }
-    }
-}
-);
-    fusioncharts.render();
-    });
 
