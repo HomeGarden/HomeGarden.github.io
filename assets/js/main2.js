6db02114fbbf88e34dfcb2b510b28292c94408d8
HomeGarden <>
2019-01-20 22:28:41 +0100

    22

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -498,8 +498,6 @@
 function initElement()
 {
   var p = document.getElementById("foo");
-  // NOTE: showAlert(); ou showAlert(param); NE fonctionne PAS ici.
-  // Il faut fournir une valeur de type function (nom de fonction déclaré ailleurs ou declaration en ligne de fonction).
   p.onclick = showAlert;
 };
 
